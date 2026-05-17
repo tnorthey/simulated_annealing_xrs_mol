7a12a92aece5723d4daddabfadb2f9548e215125
thomas <thomas.northey@pm.me>
2026-05-17 12:35:41 +0100

    run_chd_trajectory script

---
+++ run_chd_trajectory_all_from_prev.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+number_flag=$1  # 0,1,2, ...
+
+Traj=094
+Phi=0.4
+Excitation=1.0
+Qmax=8
+Top_N=200
+Description=""$number_flag""
+Results_dir="results_chd_traj_"$Traj"_phi"$Phi"_qmax"$Qmax"_"$Description""
+
+# Initialisation: Time-step 00
+i=00
+RESULTS_DIR=$Results_dir STARTING_XYZ="xyz/start.xyz" GPU_CHAINS=512 TOP_N=$Top_N TARGET_FILE="xyz/target_traj"$Traj"/target_"$i".xyz" ./scripts/bash/run_gpu_from_previous_timestep.sh $i $Excitation $Phi
+
+for i in {01..75}
+do
+    RESULTS_DIR=$Results_dir GPU_CHAINS=512 TOP_N=$Top_N TARGET_FILE="xyz/target_traj"$Traj"/target_"$i".xyz" ./scripts/bash/run_gpu_from_previous_timestep.sh $i $Excitation $Phi
+done
+
